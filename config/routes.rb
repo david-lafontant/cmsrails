e94c315b75f52f8d37a7d51dd39ba183bd22ee4f
David Lafontant <lafontant.david@gmail.com>
2024-06-23 08:14:43 -0400

    Add custom routes and page resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
+
+  resources :pages, only: [:index, :show]
+  # Defines the root path route ("/")
+   root "pages#index"
+
+  # Define admin namespace
   namespace :admin do
     resources :pages
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # Define custom routes for pages
+  Page.all.where.not(:slug => "").each do |page|
+    get "/#{page.slug}", controller: "pages", action: "show", id: page.id
+  end
+ 
 end
